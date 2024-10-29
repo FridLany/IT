e282dcfc31b4892d24be9d5f1acde84c7fc5d66e
FridLany <89321261+FridLany@users.noreply.github.com>
2024-10-29 17:54:41 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,10 @@
+name = 'Vyahceslav'
+print(name, type(name))
+name = 55
+print(name, type(name))
+Age = 17
+print (Age, type(Age))
+Age = 15
+print (Age, type(Age))
+is_student = True
+print(is_student, type(is_student))
