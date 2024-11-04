889a0e909868ca031ca573a6730d2cb9534ceb11
FridLany <89321261+FridLany@users.noreply.github.com>
2024-11-04 17:38:46 +0300

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,6 @@
+home_work = 12
+time = 1.5
+name_course = 'Python'
+print(time / home_work)
+average_execution_time = time / home_work
+print('Курс:', name_course, 'всего задач:' ,  home_work, 'затрачено часов:', time, 'среднее время выполнения:', average_execution_time, '.', sep='')
