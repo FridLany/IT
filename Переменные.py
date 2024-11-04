4cfe99936b8161e3d5a780d8594d53316a37a622
FridLany <89321261+FridLany@users.noreply.github.com>
2024-11-04 17:38:31 +0300

    Delete Переменные.py

--- Переменные.py
+++
@@ -1,6 +0,0 @@
-home_work = 12
-time = 1.5
-name_course = 'Python'
-print(time / home_work)
-average_execution_time = 0.125
-print('Курс:', name_course, 'всего задач:' ,  home_work, 'затрачено часов:', time, 'среднее время выполнения:', time / home_workD)
